2cf7dfad29f2367f659a29e06d16c786784efd71
Evgrafov Denis <stereodenis@gmail.com>
2020-04-02 18:24:58 +0300

    Fb auth

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import { AccessToken, LoginButton } from 'react-native-fbsdk'
 import { Options } from 'react-native-navigation'
 
 import styles from './styles'
@@ -7,6 +8,23 @@ import styles from './styles'
 const ProfileScreen = () => (
   <SafeAreaView style={styles.container}>
     <Text>Profile will be here soon!</Text>
+    <LoginButton
+      permissions={['public_profile']}
+      onLoginFinished={(error, result) => {
+        if (error) {
+          console.log('login has error: ' + result.error)
+        } else if (result.isCancelled) {
+          console.log('login is cancelled.')
+        } else {
+          console.log(result)
+          AccessToken.getCurrentAccessToken().then((data) => {
+            console.log(data)
+            console.log(data.accessToken.toString())
+          })
+        }
+      }}
+      onLogoutFinished={() => console.log('logout.')}
+    />
   </SafeAreaView>
 )
 
